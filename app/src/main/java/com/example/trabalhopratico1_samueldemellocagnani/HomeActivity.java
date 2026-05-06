42cff3b68782f4294bea2f4b9a91dccd282d9b78
SamuelCagnani <samuelcagnani@gmail.com>
2026-05-05 22:50:13 -0300

    new

--- app/src/main/java/com/example/trabalhopratico1_samueldemellocagnani/HomeActivity.java
+++ app/src/main/java/com/example/trabalhopratico1_samueldemellocagnani/HomeActivity.java
@@ -1,24 +1,27 @@
 package com.example.trabalhopratico1_samueldemellocagnani;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        MaterialButton btnVoltar = findViewById(R.id.btnVoltar);
+        MaterialButton btnDemanda = findViewById(R.id.btnDemanda);
+
+        btnDemanda.setOnClickListener(v -> {
+            Intent intent = new Intent(HomeActivity.this, DemandActivity.class);
+            startActivity(intent);
         });
+
+        btnVoltar.setOnClickListener(v -> finish());
     }
 }
